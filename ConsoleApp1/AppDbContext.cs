abae12210f0542adfb3bf2bd919ab9d1465bd355
sabgo <g.sabarinath81@gmail.com>
2021-07-15 05:27:28 +1000

    Add project files.

---
+++ ConsoleApp1/AppDbContext.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<LocationData> LocationData { get; set; }
+
+        public AppDbContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source = rscamalertsample.db");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<LocationData>()
+                .Property(e => e.Latitude)
+                .HasConversion<double>();
+            modelBuilder.Entity<LocationData>()
+                .Property(e => e.Longitude)
+                .HasConversion<double>();
+        }
+
+    }
+
+    public class LocationData
+    {
+        public LocationData(Guid id, double latitude, double longitude)
+        {
+            Latitude = latitude;
+            Longitude = longitude;
+            Id = id;
+        }
+
+        public Guid Id { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+    }
+
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text.Json;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            List<LocationData> locationDatas = new List<LocationData>();
+            
+            Random random = new Random();
+            //var value = lower + (random.NextDouble() * (upper - lower))
+            for (int i = 0; i < 10; i++)
+            {
+                var randDouble = random.NextDouble();
+                var latitude = -35.12 + (randDouble * -35.99 - (-35.12));
+                var longitude = 149.01 + (randDouble * 149.9999 - (149.01));                
+                locationDatas.Add(new LocationData(Guid.NewGuid(), latitude, longitude));
+            }
+
+            Console.WriteLine(JsonSerializer.Serialize(locationDatas));
+
+            var recordsInserted = Insert(locationDatas, data => new {
+                    data.Latitude,
+                    data.Longitude
+                });
+
+            Console.WriteLine("Records inserted: " + recordsInserted);
+            
+
+            Console.ReadLine();
+        }
+
+        private static int Insert(List<LocationData> items, Expression<Func<LocationData, object>> expression)
+        {
+
+            var context = new AppDbContext();
+            var resultInfo = new Z.BulkOperations.ResultInfo();
+            try
+            {
+                context.BulkInsert(items, options =>
+                {
+                    options.InsertIfNotExists = true;
+                    options.ColumnPrimaryKeyExpression = expression;
+                    options.UseRowsAffected = true;
+                    options.ResultInfo = resultInfo;
+                });
+                var rowsInserted = resultInfo.RowsAffectedInserted;
+                return rowsInserted;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw;
+            }
+            
+        }
+    }
+}
